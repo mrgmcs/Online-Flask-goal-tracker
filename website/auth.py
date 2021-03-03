9ecd76c205cdac1de13e842449562358ec47fbd3
moein <moeinrezaie1382mrg@gmail.com>
2021-03-03 14:32:02 +0330

    test for post and get methods on login and signup pages

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,38 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods = ["POST", "GET"])
 def login():
-    return render_template("login.html")
+
+    if request.method=="POST":
+        pass
+        '''
+        if user pressed Login:
+        check user's info and login
+        '''
+    else:
+        '''
+        if user opened login page with get method
+        '''
+        return render_template("login.html")
+
 
 @auth.route("/logout")
 def logout():
     return "Logout "
 
-@auth.route("/signup")
+
+
+@auth.route("/signup", methods = ["POST", "GET"])
 def signup():
-    return render_template("signup.html")
+    
+    if request.method="POST":
+        pass
+        '''
+        chek username and email and passwords , if they chould be valid, make 
+        account
+        '''
+    else:
+        return render_template("signup.html")
 
