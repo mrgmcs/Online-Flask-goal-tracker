fab28c80310cabf06cb2c8ae1909fb8b49d2ce26
moein <moeinrezaie1382mrg@gmail.com>
2021-03-06 23:43:56 +0330

    write some code to check data validation for signup page

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
@@ -10,7 +10,7 @@ def login():
         '''
         if user pressed Login:
         check user's info and login
-        '''
+        ''' 
     else:
         '''
         if user opened login page with get method
@@ -27,12 +27,40 @@ def logout():
 @auth.route("/signup", methods = ["POST", "GET"])
 def signup():
     
-    if request.method="POST":
-        pass
-        '''
-        chek username and email and passwords , if they chould be valid, make 
-        account
-        '''
+    if request.method=="POST":
+        #get data
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        # check validation of data
+
+        if len(email)<4:
+            #invalid
+            #flash("Email must be greater then 4 characters" , category="error")
+            return render_template("signup.html", massage = "Email must be greater then 4 characters")        
+        
+        elif password1 != password2:
+            #passwords are not the same
+            #flash("Passwords didn't match." , category="error")
+            return render_template("signup.html", massage = "Passwords didn't match.")
+        
+        elif len(password1)< 8 or len(password2)<8:
+            #passwords are too short
+            #flash("Passwords are too short." , category="error")
+            return render_template("signup.html", massage = "Passwords are too short.")
+        
+        elif len(username) < 3:
+            #username has no value
+            #flash("Username must be greater than 3 characters." , category="error")
+            return render_template("signup.html", massage = "Username must be greater than 3 characters.")
+        
+        
+        else:
+            #add user to database
+            #flash("Account created.", category="success")
+            return "success!"
     else:
         return render_template("signup.html")
 
